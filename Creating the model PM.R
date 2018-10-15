5c17fff3e96aefccbfee09bcb9ef66174d869c93
Phanna <phanna94@outlook.com>
2018-10-15 12:19:51 +0200

    corpus

--- Creating the model PM.R
+++ Creating the model PM.R
@@ -5,7 +5,7 @@ required_packages <- c("tm", "RTextTools","dplyr", "e1071","reshape2", "wordclou
 x <- lapply(required_packages, library, character.only = TRUE)
 
 #load RTextTools from Ronald 
-install_github("ronald245/RTextTools", subdir = "RTextTools")
+#install_github("ronald245/RTextTools", subdir = "RTextTools")
 
 #open file hotel reviews downloaded from kaggle
 setwd("D:/Data/Rstudio/Data Science/")
@@ -15,25 +15,32 @@ df <- read.csv("Hotel_Reviews.csv", stringsAsFactors = FALSE, nrow=20000)
 
 #randomize the dataset
 df <- df[sample(row(df)),]
-#df$Positive_Review <- as.vector(df$Positive_Review)
-#df$Negative_Review <- as.vector (df$Negative_Review)
-#df$combi <- paste(df$Positive_Review, df$Negative_Review)
-
-#dataframe aangemaakt gecombineerd incl ID column create as vector
 pos.rev <- as.data.frame(t(rbind(review_body = df$Positive_Review, Consensus = as.integer(1))))
 neg.rev <- as.data.frame(t(rbind(review_body = df$Negative_Review, Consensus = as.integer(-1))))
-df <- rbind(pos.rev, neg.rev)%>% tibble::rowid_to_column("ID")
+df <-rbind(pos.rev, neg.rev)
+
+#df$Positive_Review <- as.vector(df$Positive_Review)
+#df$Negative_Review <- as.vector(df$Negative_Review)
+#df$combi <- paste(df$Positive_Review, df$Negative_Review)
+
+#dataframe aangemaakt gecombineerd incl ID column create as vector  
+#df <-rbind(pos.rev, neg.rev)
+
+
+#df <- rbind(pos.rev, neg.rev)%>% tibble::rowid_to_column("ID")
+
 #df_clean$review_body <- as.vector(df_clean$review_body)
 
 #make a corpus
-hotel_corpus <- VCorpus(VectorSource(df))
+hotel_corpus <- VCorpus(VectorSource(df$review_body))
 
 #inspect corpus
 hotel_corpus
 inspect(hotel_corpus[1:3])
 content(hotel_corpus[[1]]) # check first review
 
-
+#show first item
+strwrap(hotel_corpus[1])
 
 #Corpus cleaning 
 my_stopwords <- c(stopwords("en"),"positive", "negative","available", "via", 
