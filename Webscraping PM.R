31433fd7c6c4b8dc2eec9e3626ca3f15a5037cd1
Phanna <phanna94@outlook.com>
2018-10-15 17:04:41 +0200

    Creating function for scraping

--- Webscraping PM.R
+++ Webscraping PM.R
@@ -1,8 +1,8 @@
 # loading libraries rvest, xml, stringr, mysql
-required_packages <- c("rvest", "tidyverse", "rebus", "XML", "stringr", "RMySQL", "dbConnect")
+required_packages <- c("rvest", "tidyverse", "XML", "stringr", "RMySQL", "dbConnect")
 x <- lapply(required_packages, library, character.only = TRUE)
 
-# connect to MySQL  WERKT NIET
+# connect to MySQL  
 db <- dbConnect(RMySQL::MySQL(), 
                 dbname="rstudio", 
                 host="localhost",
@@ -22,15 +22,92 @@ page <- read_html(url)
 #get reviews            https://www.datacamp.com/community/tutorials/r-web-scraping-rvest
 get_reviews <- function(html){
   page %>% 
-    # The relevant tag
+    # The relevant tag indicates the class
     html_nodes('.partial_entry') %>%      
+    #extract only the raw text as a list  
     html_text() %>% 
     # Trim additional white space
-    str_trim() %>%                       
-    # Convert the list into a vector
-    unlist()                             
+    str_trim() %>%               
+    #Extracting the url
+    #html_attr("href")
+    # Convert the list into a vector 
+    unlist() %>%
+    return()
 }
 
+get_reviews(page)
+get_reviews(next_page)
+
+#scraping next page
+getNextUrl <- function(url) {
+  page %>% 
+    html_node(".next") %>%
+    html_attr("href")
+  sep = ''
+}
+
+
 #open the next review page
-next_page <- paste(basic_url, page %>% html_node(".next") %>% html_attr("href"), sep = '')
-next_page
+more_rev <- function(url, n) {
+  purrr::map_df(1:n, ~{
+    oUrl <- url
+    post <- get_reviews(page)
+    url <<- getNextUrl(url)
+    data.frame(curpage = oUrl, 
+               nexturl = url,
+               posttext = post)
+  })
+}
+
+df <- more_rev(url,12)
+
+df$posttext[[1]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Get the titles from the reviews & Converting the titles to text
+review_titles <- append(review_titles, html_text(html_nodes(page, '.title > .noQuotes')))
+review_text <- append(review_text, html_text(html_nodes(page, '.prw_reviews_text_summary_hsx > .entry > .partial_entry')))
+
+# Create dataframe for the Reviews
+reviews <- cbind(review_titles, review_text)
+colnames(reviews) <- c('Titles', 'Review')
+
+print(reviews)
+
+if (store_reviews_in_csv) {
+  # Export the reviews table to csv file
+  write.table(reviews, 'csv/tripadvisor_ozo_hotel_reviews.csv') 
+}
+
+if (store_reviews_in_db) {
+  print('Storing reviews in database...')
+  
+  reviews[,1] <- gsub('"', "'", reviews[,1])
+  reviews[,2] <- gsub('"', "'", reviews[,2])
+  
+  # Insert Table data into the Database
+  for(rowId in c(1:nrow(reviews))) {
+    callQuery <- paste('CALL add_review("', reviews[rowId, 1], '", "', reviews[rowId,2], '")', sep = "")
+    dbSendQuery(db, callQuery)
+  }
+  
+  # Close connection
+  dbDisconnect(db)
+  
+}
+
+}
