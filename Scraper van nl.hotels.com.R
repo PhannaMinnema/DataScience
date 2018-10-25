3e033473d399c870c81f6f5e40add783cfdea9c8
Phanna <phanna94@outlook.com>
2018-10-25 16:03:10 +0200

    gets 100 reviews and scores put it into df. Don't screw this one up!

--- Scraper van nl.hotels.com.R
+++ Scraper van nl.hotels.com.R
@@ -6,38 +6,44 @@ library ('stringr')
 #install.packages ('XML')
 library ('XML')
 
-basicurl <- 'https://uk.hotels.com/ho121869'
+
 urlhotel <-paste(basicurl,'/?q-check-out=2018-10-28&FPQ=3&q-check-in=2018-10-27&WOE=7&WOD=6&q-room-0-children=0&pa=1&tab=description&JHR=2&q-room-0-adults=2&YGF=2&MGT=1&ZSX=0&SYE=3', sep = '')
 
-hotelurl <- paste(basicurl, sep='')
-hotelscrape <- read_html(hotelurl)
+hotelurl <- paste(basicurl, i, sep='')
+hotelscrape <- read_html(urlhotel)
 
-#scraper (pakt er 5)
+
+basicurl <- 'https://uk.hotels.com/ho121869-tr'
+page <- read_html(basicurl) 
+page2 <- read_html('https://uk.hotels.com/ho121869-tr-p2/')
+
+#scraper (pakt er 50)
 reviewscraper <- 
-  hotelscrape %>%
+  page %>%
   html_nodes("blockquote.expandable-content") %>% #pipe-operator
   html_text()
 
-# pakt er 5 
+# pakt er 50
 scorescraper <- 
-  hotelscrape %>%
+  page%>%
   html_nodes(".review-score .rating")%>%
   html_text()
-  
 
-nexturl <- paste(basicurl, html_attr(html_node(hotelscrape, '.next'), 'href'), sep = '')
+#pakt de volgende 50
+reviewscraper2 <- 
+  page2 %>%
+  html_nodes("blockquote.expandable-content") %>% #pipe-operator
+  html_text()
 
-#reviews <- function(hotelscrape, n){
-for (i in 1:20){
-    review <- reviewscraper 
-    score <- scorescraper 
-    nextpage <- nexturl
-    df <- data.frame(
-      review,
-      score)
-}
+# pakt de volgende 50
+scorescraper2 <- 
+  page2 %>%
+  html_nodes(".review-score .rating")%>%
+  html_text()
 
+df1 <- cbind(score = scorescraper, review = reviewscraper)
+df2 <- cbind (score = scorescraper2, review=reviewscraper2)
 
-
+df <- data.frame(rbind(df1, df2))
 
 
