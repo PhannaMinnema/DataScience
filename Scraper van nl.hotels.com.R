e9b471388e85c0dbd81a2d391597356b99df0f39
Phanna <phanna94@outlook.com>
2018-10-25 15:07:37 +0200

    creates df, but only for 5 reviews

--- Scraper van nl.hotels.com.R
+++ Scraper van nl.hotels.com.R
@@ -6,22 +6,38 @@ library ('stringr')
 #install.packages ('XML')
 library ('XML')
 
+basicurl <- 'https://uk.hotels.com/ho121869'
+urlhotel <-paste(basicurl,'/?q-check-out=2018-10-28&FPQ=3&q-check-in=2018-10-27&WOE=7&WOD=6&q-room-0-children=0&pa=1&tab=description&JHR=2&q-room-0-adults=2&YGF=2&MGT=1&ZSX=0&SYE=3', sep = '')
 
-urlhotel <-url('https://nl.hotels.com/ho149565/?q-check-out=2018-10-28&FPQ=5&q-check-in=2018-10-27&WOE=7&WOD=6&q-room-0-children=0&pa=1&tab=description&JHR=4&q-room-0-adults=2&YGF=2&MGT=1&ZSX=0&SYE=3')
-
-hotelscrape <- read_html(urlhotel)
+hotelurl <- paste(basicurl, sep='')
+hotelscrape <- read_html(hotelurl)
 
 #scraper (pakt er 5)
-reviewscraper <- hotelscrape %>%
+reviewscraper <- 
+  hotelscrape %>%
   html_nodes("blockquote.expandable-content") %>% #pipe-operator
   html_text()
-reviewscraper
 
 # pakt er 5 
-scorescraper <- hotelscrape%>%
+scorescraper <- 
+  hotelscrape %>%
   html_nodes(".review-score .rating")%>%
   html_text()
-scorescraper
+  
 
-df <- data.frame(reviewscraper, scorescraper)
+nexturl <- paste(basicurl, html_attr(html_node(hotelscrape, '.next'), 'href'), sep = '')
+
+#reviews <- function(hotelscrape, n){
+for (i in 1:20){
+    review <- reviewscraper 
+    score <- scorescraper 
+    nextpage <- nexturl
+    df <- data.frame(
+      review,
+      score)
+}
+
+
+
+
 
