7548eeacda2c418fec96945f608c2c087c226ed2
Phanna <phanna94@outlook.com>
2018-11-06 21:44:59 +0100

    Working hotelscraper with cleaner and MySQL function 250 reviews

--- Scraper van nl.hotels.com.R
+++ Scraper van nl.hotels.com.R
@@ -5,31 +5,29 @@ library('rvest')
 library ('stringr')
 #install.packages ('XML')
 library ('XML')
+library ('RMySQL')
+library('dplyr')
 
-# connect to MySQL  
-db <- dbConnect(RMySQL::MySQL(), 
-                dbname="rstudio", 
-                host="localhost",
-                port= 3306,
-                user="PM",
-                password="password")
-
-
+setwd("D:/Data/Github/DataScience/")
 
 urlhotel <-paste(basicurl,'/?q-check-out=2018-10-28&FPQ=3&q-check-in=2018-10-27&WOE=7&WOD=6&q-room-0-children=0&pa=1&tab=description&JHR=2&q-room-0-adults=2&YGF=2&MGT=1&ZSX=0&SYE=3', sep = '')
 
 hotelurl <- paste(basicurl, i, sep='')
 hotelscrape <- read_html(urlhotel)
 
-basicurl <- 'https://uk.hotels.com/ho121869-tr'
+basicurl <- 'https://uk.hotels.com/ho121869-tr-p1'
 page <- read_html(basicurl) 
 page2 <- read_html('https://uk.hotels.com/ho121869-tr-p2/')
+page3 <- read_html("https://uk.hotels.com/ho121869-tr-p3/")
+page4 <- read_html("https://uk.hotels.com/ho121869-tr-p4/")
+page5 <- read_html("https://uk.hotels.com/ho121869-tr-p5/")
 
 #scraper (pakt er 50)
 reviewscraper <- 
   page %>%
   html_nodes("blockquote.expandable-content") %>% #pipe-operator
   html_text()
+
 
 # pakt er 50
 scorescraper <- 
@@ -49,16 +47,72 @@ scorescraper2 <-
   html_nodes(".review-score .rating")%>%
   html_text()
 
-df1 <- cbind(score = scorescraper, review = reviewscraper)
-df2 <- cbind (score = scorescraper2, review=reviewscraper2)
+#pakt de volgende 50
+reviewscraper3 <- 
+  page3 %>%
+  html_nodes("blockquote.expandable-content") %>% #pipe-operator
+  html_text()
 
-df <- data.frame(rbind(df1, df2))
+# pakt de volgende 50
+scorescraper3 <- 
+  page3 %>%
+  html_nodes(".review-score .rating")%>%
+  html_text()
+
+#pakt de volgende 50
+reviewscraper4 <- 
+  page4 %>%
+  html_nodes("blockquote.expandable-content") %>% #pipe-operator
+  html_text()
+
+# pakt de volgende 50
+scorescraper4 <- 
+  page4 %>%
+  html_nodes(".review-score .rating")%>%
+  html_text()
+
+#pakt de volgende 50
+reviewscraper5 <- 
+  page5 %>%
+  html_nodes("blockquote.expandable-content") %>% #pipe-operator
+  html_text()
+
+# pakt de volgende 50
+scorescraper5 <- 
+  page5 %>%
+  html_nodes(".review-score .rating")%>%
+  html_text()
+
+
+df.webreviews <- data.frame(cbind(score= c(scorescraper,scorescraper2, scorescraper3, scorescraper4, scorescraper5), review=c(reviewscraper, reviewscraper2, reviewscraper3, reviewscraper4, reviewscraper5)))
+df.webreviews$score <- as.numeric(as.character(df.webreviews$score))
+df.webreviews$Consensus <- ifelse(df.webreviews$score <6, -1,
+                                  ifelse(df.webreviews$score >6 && df.webreviews$score<8, 0, 1))
+
+df.webreviews$score <- NULL
+df.webreviews$Consensus <- as.factor(df.webreviews$Consensus)
+
+rm(reviewscraper, reviewscraper2, reviewscraper3, reviewscraper4,reviewscraper5, scorescraper, scorescraper2, scorescraper3, scorescraper4, scorescraper5)
+
+source("Cleaner.R")
+
+df.webreviews$review <- CleanBody(df.webreviews$review)
+
+#
+db <- dbConnect(RMySQL::MySQL(), 
+                dbname="rstudio", 
+                host="localhost",
+                port= 3306,
+                user="PM",
+                password="password")
 
 
 # put data into MySQL with overwrite function  
 SaveDataFrameToDB <- function(db, table, df, doAppend){
-  dbWriteTable(db, table, df, overwrite = !doAppend, append=doAppend)
+  dbWriteTable(db, table, df.webreviews, overwrite = !doAppend, append=doAppend)
   dbDisconnect(db)
 }
 
-SaveDataFrameToDB(db, "webreviews",df, doAppend=TRUE)
+SaveDataFrameToDB(db, "webreviews",df.webreviews, doAppend=TRUE)
+
+ 
